018ce0b783d5187722f97b72e789f2c40f0578ff
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-11-25 21:55:38 -0300

    Implementação da renderização condicional de listas no Vue.js. Agora a lista é exibida corretamente e uma mensagem de lista vazia é mostrada quando necessário

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -16,12 +16,15 @@ export default {
             <span class="subtitulo-lg sua-lista-texto">
                 Tua lista:
             </span>
-            <ul class="ingredientes-sua-lista">
+            <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
                 <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
                     {{ ingrediente }}
                 </li>
-
             </ul>
+            <p v-else class="paragrafo lista-vazia">
+                <img src="../assets/images/icones/lista-vazia.svg" alt="Icone de pesquisa">
+                Sua lista está vazia, selecione ingredientes para iniciar.
+            </p>
         </section>
     </main>
 </template>
